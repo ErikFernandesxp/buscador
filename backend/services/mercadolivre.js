11c6cb9dec10e1488334b796ef3d3cf851dcdfc5
ErikFernandesxp <gamermito222@gmail.com>
2026-05-04 00:36:19 -0300

    Update mercadolivre.js

--- backend/services/mercadolivre.js
+++ backend/services/mercadolivre.js
@@ -1,14 +1,19 @@
 const axios = require('axios');
 
 exports.search = async (q) => {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${q}`;
-  const res = await axios.get(url);
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${q}`;
+    const res = await axios.get(url);
 
-  return res.data.results.slice(0, 20).map(i => ({
-    title: i.title,
-    price: i.price,
-    image: i.thumbnail ? i.thumbnail.replace('-I.jpg', '-O.jpg') : "",
-    link: i.permalink,
-    source: "Mercado Livre"
-  }));
+    return res.data.results.slice(0, 10).map(i => ({
+      title: i.title,
+      price: i.price,
+      image: i.thumbnail?.replace('-I.jpg', '-O.jpg') || "",
+      link: i.permalink,
+      source: "Mercado Livre"
+    }));
+
+  } catch (err) {
+    return [];
+  }
 };
